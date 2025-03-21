4a81aa8a6a129eb651ebeb4e8baa15e11d1e9bd6
Thien_Lang <kieutantai2x@gmail.com>
2025-03-21 14:44:00 +0700

    add 3 button borrow, return, reserve

--- QuanLyThuQuan/GUI/FormTransaction.Designer.cs
+++ QuanLyThuQuan/GUI/FormTransaction.Designer.cs
@@ -29,27 +29,98 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.btnReturnBook = new System.Windows.Forms.Button();
+            this.btnBookReservation = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(353, 161);
+            this.label1.Location = new System.Drawing.Point(449, 311);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Panel Transaction";
             // 
+            // btnBorrow
+            // 
+            this.btnBorrow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBorrow.ForeColor = System.Drawing.Color.Black;
+            this.btnBorrow.Location = new System.Drawing.Point(3, 3);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(313, 94);
+            this.btnBorrow.TabIndex = 1;
+            this.btnBorrow.Text = "Borrow Book";
+            this.btnBorrow.UseVisualStyleBackColor = true;
+            this.btnBorrow.MouseEnter += new System.EventHandler(this.btnBorrow_MouseEnter);
+            this.btnBorrow.MouseLeave += new System.EventHandler(this.btnBorrow_MouseLeave);
+            // 
+            // btnReturnBook
+            // 
+            this.btnReturnBook.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReturnBook.ForeColor = System.Drawing.Color.Black;
+            this.btnReturnBook.Location = new System.Drawing.Point(323, 3);
+            this.btnReturnBook.Name = "btnReturnBook";
+            this.btnReturnBook.Size = new System.Drawing.Size(313, 94);
+            this.btnReturnBook.TabIndex = 2;
+            this.btnReturnBook.Text = "Return Book";
+            this.btnReturnBook.UseVisualStyleBackColor = true;
+            this.btnReturnBook.Click += new System.EventHandler(this.button2_Click);
+            this.btnReturnBook.MouseEnter += new System.EventHandler(this.btnReturnBook_MouseEnter);
+            this.btnReturnBook.MouseLeave += new System.EventHandler(this.btnReturnBook_MouseLeave);
+            // 
+            // btnBookReservation
+            // 
+            this.btnBookReservation.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBookReservation.ForeColor = System.Drawing.Color.Black;
+            this.btnBookReservation.Location = new System.Drawing.Point(643, 3);
+            this.btnBookReservation.Name = "btnBookReservation";
+            this.btnBookReservation.Size = new System.Drawing.Size(314, 94);
+            this.btnBookReservation.TabIndex = 3;
+            this.btnBookReservation.Text = "Book Reservation";
+            this.btnBookReservation.UseVisualStyleBackColor = true;
+            this.btnBookReservation.Click += new System.EventHandler(this.btnHandleViolations_Click);
+            this.btnBookReservation.MouseEnter += new System.EventHandler(this.btnBookReservation_MouseEnter);
+            this.btnBookReservation.MouseLeave += new System.EventHandler(this.btnBookReservation_MouseLeave);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.btnReturnBook, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnBookReservation, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnBorrow, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(960, 100);
+            this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
             // FormTransaction
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(960, 650);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormTransaction";
             this.Text = "FormTransaction";
             this.Load += new System.EventHandler(this.FormTransaction_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +129,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBorrow;
+        private System.Windows.Forms.Button btnReturnBook;
+        private System.Windows.Forms.Button btnBookReservation;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- QuanLyThuQuan/GUI/FormTransaction.cs
+++ QuanLyThuQuan/GUI/FormTransaction.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace QuanLyThuQuan.GUI
 {
-    public partial class FormTransaction: Form
+    public partial class FormTransaction : Form
     {
+
+        // private methods
+        private void ChangeColorHoverBtn(Button button, Color color)
+        {
+            button.BackColor = color;
+        }
+
         public FormTransaction()
         {
             InitializeComponent();
@@ -20,6 +21,51 @@ namespace QuanLyThuQuan.GUI
         private void FormTransaction_Load(object sender, EventArgs e)
         {
             this.ControlBox = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHandleViolations_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnBorrow_MouseEnter(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnBorrow, Color.LightCyan);
+        }
+
+        private void btnReturnBook_MouseEnter(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnReturnBook, Color.LightCyan);
+        }
+
+        private void btnBookReservation_MouseEnter(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnBookReservation, Color.LightCyan);
+        }
+
+        private void btnBorrow_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnBorrow, Color.Transparent);
+        }
+
+        private void btnReturnBook_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnReturnBook, Color.Transparent);
+        }
+
+        private void btnBookReservation_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColorHoverBtn(btnBookReservation, Color.Transparent);
         }
     }
 }
