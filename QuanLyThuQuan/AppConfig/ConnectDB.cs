c3a2e4b53f29591afd29e5806ba858b72d8f0fc3
ThoiThanhVuong <Thanhvuong0419@gmail.com>
2025-04-02 09:24:09 +0700

    connect mysql

---
+++ QuanLyThuQuan/AppConfig/ConnectDB.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace QuanLyThuQuan.AppConfig
+{
+    class ConnectDB
+    {
+        private string connectionString = "server=localhost;port=3306;database=QuanLyThuQuan;user=root;password=;";
+        private MySqlConnection connection;
+
+        public ConnectDB()
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        // Mở kết nối MySQL
+        public void OpenConnection()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    Console.WriteLine("Kết nối MySQL thành công!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi kết nối: " + ex.Message);
+            }
+        }
+
+        // Đóng kết nối MySQL
+        public void CloseConnection()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("Đã đóng kết nối MySQL!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi đóng kết nối: " + ex.Message);
+            }
+        }
+
+
+    }
+}
--- QuanLyThuQuan/DAO/BookDAO.cs
+++ QuanLyThuQuan/DAO/BookDAO.cs
@@ -1,6 +1,16 @@
-﻿namespace QuanLyThuQuan.DAO
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using QuanLyThuQuan.AppConfig;
+namespace QuanLyThuQuan.DAO
 {
     class BookDAO
     {
+        private ConnectDB connectDB;
+        public BookDAO()
+        {
+            connectDB = new ConnectDB(); 
+        }
+
     }
 }
